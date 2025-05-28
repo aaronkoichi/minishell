4653ee7d1c4ffa05bb8891bbbf0833055ac6e98c
Zhe Tse Lee <zlee@u81z05s04.42kl.edu.my>
2025-05-28 18:00:26 +0800

    fix cd

--- srcs/exec_builtin_func.c
+++ srcs/exec_builtin_func.c
@@ -6,15 +6,17 @@
 /*   By: zlee <zlee@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/28 14:38:46 by zlee              #+#    #+#             */
-/*   Updated: 2025/05/28 17:22:24 by zlee             ###   ########.fr       */
+/*   Updated: 2025/05/28 18:00:05 by zlee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 #include "execute.h"
+#include <dirent.h>
 
 static int	exit_func(char **cmd)
 {
+	printf("exit\n");
 	if (cmd[1] != NULL)
 		exit(ft_atoi(cmd[1]));
 	exit(EXIT_SUCCESS);
@@ -22,18 +24,24 @@ static int	exit_func(char **cmd)
 
 static int	cd_func(char **cmd, char **envp)
 {
-	int	i;
+	int		i;
+	char	*ptr;
 
+	ptr = NULL;
 	i = -1;
 	if (cmd[1] != NULL)
-		chdir(cmd[1]);
+		ptr = cmd[1];
 	else
 	{
 		while(envp[++i])
 			if (!ft_strncmp("HOME=", envp[i], 5))
 				break ;
-		chdir(envp[i] + 5);
+		ptr = &envp[i][5];
 	}
+	if (opendir(ptr) == NULL)
+		perror("cd");
+	else
+		chdir(ptr);
 	return (0);
 }
 
