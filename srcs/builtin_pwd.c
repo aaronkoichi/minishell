4be9fd31164f1bbfbb9a9ae72705d209501faa1a
aaronkoichi <aaronlee4399@gmail.com>
2025-07-02 13:19:35 +0800

    fix getcwd leak

--- srcs/builtin_pwd.c
+++ srcs/builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: jthiew <jthiew@student.42kl.edu.my>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/31 16:16:33 by jthiew            #+#    #+#             */
-/*   Updated: 2025/07/02 12:40:40 by zlee             ###   ########.fr       */
+/*   Updated: 2025/07/02 13:18:43 by zlee             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ int	builtin_pwd(t_cmd *cmd, t_env **env, t_vars *vars, t_str_dat dat)
 		return (1);
 	}
 	ft_putstr_fd(cwd, 1);
+	free(cwd);
 	ft_putstr_fd("\n", 1);
 	return (0);
 }
