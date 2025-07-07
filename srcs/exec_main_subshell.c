1779a5a797af297ca3336515578f1743933980f4
Jih Herng Thiew <jthiew@u81z03s04.42kl.edu.my>
2025-07-07 10:44:22 +0800

    moved redirection inside subshell child process to not distrupt parent fds

--- srcs/exec_main_subshell.c
+++ srcs/exec_main_subshell.c
@@ -6,7 +6,7 @@
 /*   By: zlee <zlee@student.42kl.edu.my>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/01 23:35:40 by zlee              #+#    #+#             */
-/*   Updated: 2025/07/07 09:09:50 by jthiew           ###   ########.fr       */
+/*   Updated: 2025/07/07 10:43:18 by jthiew           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,25 +26,24 @@ int	function_tree_subshell(t_ast *node, t_vars *vars, t_ast *tree,
 	t_exec	info;
 
 	memset(&info, 0, sizeof(t_exec));
-	if (node->cmd->redir_count != 0)
-	{
-		if (subshell_redir(node) == EXIT_FAILURE)
-		{
-			reset_fd(vars);
-			return (EXIT_FAILURE);
-		}
-	}
 	info.fork_pid[0] = fork();
 	if (info.fork_pid[0] < 0)
 		perror("subshell\n");
 	if (info.fork_pid[0] == 0)
 	{
+		if (node->cmd->redir_count != 0)
+		{
+			if (subshell_redir(node) == EXIT_FAILURE)
+			{
+				reset_fd(vars);
+				return (EXIT_FAILURE);
+			}
+		}
 		info.status = exec_main(node->left, vars, tree, token);
 		clean_subshell(tree, token, vars);
 		exit(info.status);
 	}
 	waitpid(info.fork_pid[0], &info.status, 0);
-	reset_fd(vars);
 	vars->exit_code = WEXITSTATUS(info.status);
 	return (info.status);
 }
